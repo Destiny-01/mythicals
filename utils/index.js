ea4ea5213e0d06ba13948ebc700e3c2adca65d95
Destiny Aigbe <aigbedestinyic@gmail.com>
2023-07-15 11:23:56 +0100

    fix guessing issue

--- utils/index.js
+++ utils/index.js
@@ -8,11 +8,13 @@ exports.getStatus = (guess, solution) => {
     if (guessArr[i] === solutionArr[i]) {
       result += "dead ";
     } else if (solutionArr.includes(guessArr[i])) {
+      result += "_";
       injured++;
     } else {
-      result += "safe ";
+      result += "_ ";
     }
   }
+  console.log(result.trim(), injured, guess, solution);
 
   return { status: result.trim(), injured };
 };
